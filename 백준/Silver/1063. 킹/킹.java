d17db46fcd3a5bd9080befb734ec7f645a014f7a
yeeyo <108002997+Jimin730@users.noreply.github.com>
2024-12-28 22:00:17 +0900

    [Silver III] Title: 킹, Time: 132 ms, Memory: 16628 KB -BaekjoonHub

--- 백준/Silver/1063. 킹/킹.java
+++ 백준/Silver/1063. 킹/킹.java
@@ -1,41 +1,36 @@
 
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+
 public class Main {
 
-    static String king;
-    static String stone;
-    static int N;
-             //R,L,B,T,RT,LT,RB,LB
-    static int[] dx = {1,-1,0,0,1,-1,1,-1};
-    static int[] dy = {0,0,-1,1,1,1,-1,-1};
+    public static String king;
+    public static String stone;
+    public static int N;
+    public static int[] dx = {1,-1,0,0,1,-1,1,-1}; //R,L,B,T,RT,LT,RB,LB
+    public static int[] dy = {0,0,-1,1,1,1,-1,-1};
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        king = st.nextToken(); //king 위치
-        stone = st.nextToken(); //돌 위치
-        N = Integer.parseInt(st.nextToken()); //반복할 위치 이동의 수
+        king = st.nextToken();
+        stone = st.nextToken();
+        N = Integer.parseInt(st.nextToken());
 
-        //king 위치. 아스키 코드 기준으로 변환
-        int kcol = king.charAt(0) - 'A' + 1;
-        int krow = king.charAt(1) - '0';
+        int kx = king.charAt(0);
+        int ky = king.charAt(1) - '0';
 
-        //stone 위치. 아스키 코드 기준으로 변환
-        int scol = stone.charAt(0) - 'A' + 1;
-        int srow = stone.charAt(1) - '0';
+        int sx = stone.charAt(0);
+        int sy = stone.charAt(1) - '0';
 
-        //N만큼 반복하여 이동 수행
-        for(int i=0; i<N; i++){
-            String str = br.readLine();
-            int idx = -1;
-            //이동 전 king 위치 저장
-            int tmpx = kcol;
-            int tmpy = krow;
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine().trim();
+            int idx = -1; //초기값
+
             switch (str){
                 case "R": idx = 0; break;
                 case "L": idx = 1; break;
@@ -46,36 +41,30 @@ public class Main {
                 case "RB": idx = 6; break;
                 case "LB": idx = 7; break;
             }
-            //해당 위치로 king 이동
-            tmpx += dx[idx];
-            tmpy += dy[idx];
 
-            if(isRange(tmpx, tmpy)){ //king 이동 시 범위 안에 있는지 확인
-                if(tmpx == scol && tmpy == srow) { //이동한 king 위치에 돌이 있을 경우
-                    if(isRange(scol+dx[idx], srow+dy[idx])){ //돌도 위치를 옮겨주어야 하니, 이동한 돌이 범위 안에 있을 경우에만 실행
-                        scol += dx[idx];
-                        srow += dy[idx];
-                    } else continue;
+            if(check(kx+dx[idx], ky+dy[idx])){ //k이 움직일 위치가 범위 안에 있으면
+                if(sx == kx+dx[idx] && sy == ky+dy[idx]){ //k가 움직일 위치에 돌이 있으면
+                    if(check(sx+dx[idx], sy+dy[idx])){ //돌도 같이 움직여 줘야 하며, 그 위치가 범위 안에 있으면
+                        kx += dx[idx];
+                        ky += dy[idx];
+                        sx += dx[idx];
+                        sy += dy[idx];
+                    }
+                } else { //왕이 움직일 위치에 돌이 없으면 그냥 왕만 움직여준다.
+                    kx += dx[idx];
+                    ky += dy[idx];
                 }
-                //이동한 위치 값으로 변경
-                kcol = tmpx;
-                krow = tmpy;
             }
         }
 
-        king = "";
-        stone = "";
-
-        //결과 출력을 위해 다시 문제 좌표로 변환
-        king += Character.toString((char)(kcol+'A'-1)) + krow;
-        stone += Character.toString((char)(scol+'A'-1)) + srow;
+        king = (char)kx+String.valueOf(ky);
+        stone = (char)sx+String.valueOf(sy);
 
         System.out.println(king);
         System.out.println(stone);
-
     }
 
-    private static boolean isRange(int r, int c){ //체스판의 범위 내에 있는 지 체크
-        return r>=1 && c>=1 && r<=8 && c<=8;
+    public static boolean check(int x1, int y1){ //범위 안에 있는지 체크
+        return x1 >= 65 && y1 >= 1 && x1 <=72 && y1 <=8;
     }
 }

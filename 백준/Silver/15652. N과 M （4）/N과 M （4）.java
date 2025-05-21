ef455496812fef60eb8604fd133d508d0eac23c8
yeeyo <108002997+Jimin730@users.noreply.github.com>
2025-05-21 13:55:43 +0900

    [Silver III] Title: N과 M (4), Time: 140 ms, Memory: 17544 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 백준 15652번
+public class Main {
+
+    static int N, M;
+    static int[] ans;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        ans = new int[N+1];
+
+        go(0, 1);
+
+        System.out.println(sb);
+    }
+
+    static void go(int idx, int pos) {
+        if(idx == M) { // idx 가 마지막 위치에 도달하면 수열 출력
+            for(int i=0; i<M; i++) {
+                sb.append(ans[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=1; i<=N; i++) { // 1부터 N까지 자연수 선택
+
+            if(i<pos) continue; // 현재 수가 이전 수보다 작으면 오름차순 조건에 맞지 않음
+
+            ans[idx] = i; // 해당 위치에 i 넣음
+            pos = i; // 이전 수를 확인하기 위한 기록
+
+            go(idx + 1, pos); // 위치 + 1 후 재귀
+
+        }
+    }
+}

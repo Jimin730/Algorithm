ae6772606cc46f6a5409498d2afdf5d7fe1a5bb0
yeeyo <108002997+Jimin730@users.noreply.github.com>
2025-05-23 15:31:53 +0900

    [Silver III] Title: N과 M (6), Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 백준 15654
+public class Main {
+
+    static int N, M;
+    static int[] num;
+    static int[] ans;
+    static boolean[] check;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        num = new int[N];
+        ans = new int[N+1];
+        check = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(num);
+
+        go(0, 0);
+
+        System.out.println(sb);
+    }
+
+    static void go(int idx, int pos) {
+
+        if(idx == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(ans[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=0; i<num.length; i++) {
+
+            if(check[i]) continue;
+            if(num[i] < pos) continue;
+
+            check[i] = true;
+            ans[idx] = num[i];
+            pos = num[i];
+
+            go(idx + 1, pos);
+
+            check[i] = false;
+        }
+    }
+}

1ad5a02228a6cfd3b67b780603e9a9a00377bf25
yeeyo <108002997+Jimin730@users.noreply.github.com>
2026-02-02 14:38:11 +0900

    [Silver II] Title: N과 M (9), Time: 172 ms, Memory: 21128 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    private static int N, M;
+    private static int[] number;
+    private static boolean[] isUsed;
+    private static int[] result;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        number = new int[N];
+        isUsed = new boolean[N];
+        result = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            number[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(number);
+
+        go(0);
+
+        System.out.println(sb);
+    }
+
+    private static void go(int index) {
+        if(index == M) {
+            for(int i=0; i<M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int lastUsed = -1;
+        for(int i=0; i<N; i++) {
+            if(isUsed[i] || number[i] == lastUsed) continue;
+            
+            isUsed[i] = true;
+            result[index] = number[i];
+            lastUsed = number[i];
+
+            go(index+1);
+            
+            isUsed[i] = false;
+        }
+    }
+}
